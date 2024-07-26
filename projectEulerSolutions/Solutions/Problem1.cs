f3efbe3c45832e2f27f19596cf4c9daab8affbbd
trynfulf <amdymul@gmail.com>
2024-07-26 15:26:15 +0100

    Problem 2 solved.

--- projectEulerSolutions/Solutions/Problem1.cs
+++ projectEulerSolutions/Solutions/Problem1.cs
@@ -8,13 +8,12 @@ namespace projectEulerSolutions.Solutions
 {
 
     /// <summary>
-    /// Solves Problem1 on Project Euler. The problem is defined below: 
+    /// Solves Problem 1 on Project Euler. The problem is defined below: 
     /// 
     /// Problem 1: "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
     ///             The sum of these multiples is 23. 
     ///             
     ///             Find the sum of all the mutiples of 3 or 5 below 1000."
-    /// 
     /// </summary>
     static class Problem1
     {
